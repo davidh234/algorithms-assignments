9d5b5c95d54b93314250595182b84b3cbdd73bf7
davidh234 <davidhooban@hotmail.com>
2019-02-07 17:14:18 +0000

    Update SortComparison.java

--- SortComparison.java
+++ SortComparison.java
@@ -91,10 +91,10 @@
     				smallestPos = k;
     			}	
     		}
-			double temp = a[i];
-			a[i] = min;
-			a[smallestPos] = temp;
-			min = Double.MAX_VALUE;
+		double temp = a[i];
+		a[i] = min;
+		a[smallestPos] = temp;
+		min = Double.MAX_VALUE;
     	}
     	return a;
     }
